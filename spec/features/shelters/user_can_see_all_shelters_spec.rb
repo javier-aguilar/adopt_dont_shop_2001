9da65aa3d1957216cb38eb4e65be04ab5d408d66
Javier Aguilar <jvr.agx@gmail.com>
2020-03-17 21:44:35 -0600

    Add shelter/id view

--- spec/features/shelters/user_can_see_all_shelters_spec.rb
+++ spec/features/shelters/user_can_see_all_shelters_spec.rb
@@ -24,3 +24,41 @@ RSpec.describe "shelters index page", type: :feature do
     expect(page).to have_content(shelter_3.name)
   end
 end
+
+RSpec.describe "shelters id page", type: :feature do
+  it "can see all shelter information" do
+    shelter_1 = Shelter.create(name: "Pallet Town Shelter",
+                        address: "Route 1",
+                        city:  "Pallet Town",
+                        state: "Kanto",
+                        zip: "80807")
+    shelter_2 = Shelter.create(name: "Cerulean City Center",
+                        address: "Route 5",
+                        city:  "Cerulean City",
+                        state: "Kanto",
+                        zip: "80808")
+    shelter_3 = Shelter.create(name: "Safari Zone Rescue",
+                        address: "Route 48",
+                        city:  "Fuchsia City",
+                        state: "Kanto",
+                        zip: "80809")
+    visit "/shelters/#{Shelter.first.id}"
+
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content("Address: #{shelter_1.address}")
+    expect(page).to have_content("City: #{shelter_1.city}")
+    expect(page).to have_content("State: #{shelter_1.state}")
+    expect(page).to have_content("Zip: #{shelter_1.zip}")
+    # expect(page).to have_content(shelter_2.name)
+    # expect(page).to have_content("Address: #{shelter_2.address}")
+    # expect(page).to have_content("City: #{shelter_2.city}")
+    # expect(page).to have_content("State: #{shelter_2.state}")
+    # expect(page).to have_content("Zip: #{shelter_2.zip}")
+    # expect(page).to have_content(shelter_3.name)
+    # expect(page).to have_content("Address: #{shelter_3.address}")
+    # expect(page).to have_content("City: #{shelter_3.city}")
+    # expect(page).to have_content("State: #{shelter_3.state}")
+    # expect(page).to have_content("Zip: #{shelter_3.zip}")
+  end
+end
+

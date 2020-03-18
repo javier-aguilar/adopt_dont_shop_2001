d6cb7dfcdb3bccdbf26387e37b8b1d4516d792b6
Javier Aguilar <jvr.agx@gmail.com>
2020-03-18 15:09:45 -0600

    Add form to create shelter

--- spec/features/shelters/user_can_create_shelter_spec.rb
+++ spec/features/shelters/user_can_create_shelter_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe "shelters index page", type: :feature do
     expect(page).to have_content("New Shelter")
   end
 
+  it "can see form to create shelter" do
+    visit "/shelters/new"
+
+    expect(page).to have_content("Enter a name:")
+    expect(page).to have_content("Enter an address:")
+    expect(page).to have_content("Enter a city:")
+    expect(page).to have_content("Enter a state:")
+    expect(page).to have_content("Enter a zip:")
+  end
 end

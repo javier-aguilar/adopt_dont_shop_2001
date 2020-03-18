ff84eb304f8934907daf59ba4f53aba5d38732e9
Javier Aguilar <jvr.agx@gmail.com>
2020-03-18 16:24:53 -0600

    Add update method to Shelter

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
+  get '/', to: 'shelters#index'
   get '/shelters', to: 'shelters#index'
   post '/shelters', to: 'shelters#create'
   get '/shelters/new', to: 'shelters#new'
   get '/shelters/:id', to: 'shelters#show'
   get '/shelters/:id/edit', to: 'shelters#edit'
+  patch '/shelters/:id', to: 'shelters#update'
 end

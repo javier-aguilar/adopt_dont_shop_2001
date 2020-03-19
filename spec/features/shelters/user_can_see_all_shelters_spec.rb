b13e5507fa835bee6312ed3852c9acc829722b26
Javier Aguilar <jvr.agx@gmail.com>
2020-03-18 19:10:53 -0600

    Move test to user_can_edit_shelter_spec.rb

--- spec/features/shelters/user_can_see_all_shelters_spec.rb
+++ spec/features/shelters/user_can_see_all_shelters_spec.rb
@@ -55,17 +55,5 @@ RSpec.describe "shelters id page", type: :feature do
     expect(page).to have_content("Zip: #{shelter_2.zip}")
 
   end
-
-  it "can see link to update shelter" do
-    shelter_1 = Shelter.create(name: "Pallet Town Shelter",
-                        address: "Route 1",
-                        city:  "Pallet Town",
-                        state: "Kanto",
-                        zip: "80807")
-
-    visit "/shelters/#{shelter_1.id}"
-
-    expect(page).to have_content("Update Shelter")
-  end
 end
 

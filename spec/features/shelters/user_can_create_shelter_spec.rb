f642cc70cf5f09ca0a45b8256d436006452e98f1
Javier Aguilar <jvr.agx@gmail.com>
2020-03-18 19:32:32 -0600

    Re-write test creates a Shelter and redirects to the shelters index page

--- spec/features/shelters/user_can_create_shelter_spec.rb
+++ spec/features/shelters/user_can_create_shelter_spec.rb
@@ -7,28 +7,31 @@ RSpec.describe "shelters index page", type: :feature do
     expect(page).to have_content("New Shelter")
   end
 
-  it "can see form to create shelter" do
-    visit "/shelters/new"
+  it "can click link then see form to create shelter" do
+    visit "/shelters/"
 
+    click_link('New Shelter')
+
+    expect(page).to have_current_path("/shelters/new")
     expect(page).to have_content("Enter a name:")
     expect(page).to have_content("Enter an address:")
     expect(page).to have_content("Enter a city:")
     expect(page).to have_content("Enter a state:")
     expect(page).to have_content("Enter a zip:")
   end
-end
 
-RSpec.describe "shelters index page", :type => :request do
-  it "creates a Shelter and redirects to the Shelters index" do
-    post "/shelters", :params => {
-                        :shelter => {
-                            name: "Lavender Town Rescue",
-                            address: "Route 66",
-                            city:  "Lavender Town",
-                            state: "Kanto",
-                            zip: "80810"
-                            }
-                          }
-    expect(response).to redirect_to("/shelters")
+  it "creates a Shelter and redirects to the shelters index page" do
+    visit "/shelters/new"
+
+    fill_in('shelter[name]', :with => "Lavender Town Rescue")
+    fill_in('shelter[address]', :with => "Route 66")
+    fill_in('shelter[city]', :with => "Lavender Town")
+    fill_in('shelter[state]', :with => "Kanto")
+    fill_in('shelter[zip]', :with => "80810")
+
+    click_button('Create Shelter')
+
+    expect(page).to have_current_path("/shelters")
+    expect(page).to have_content("Lavender Town Rescue")
   end
 end

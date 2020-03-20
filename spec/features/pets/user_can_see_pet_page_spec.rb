60aa9b2e10309a83e9425ca19dc150434e9fb615
Javier Aguilar <jvr.agx@gmail.com>
2020-03-19 19:25:42 -0600

    Add show page for pets

---
+++ spec/features/pets/user_can_see_pet_page_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor", type: :feature do
+  it "can see pet information" do
+    shelter_1 = Shelter.create(name: "Pallet Town Shelter",
+                        address: "Route 1",
+                        city:  "Pallet Town",
+                        state: "Kanto",
+                        zip: "80807")
+
+    pet_1 = Pet.create(image: "pidgey.jpg",
+                        name: "Pidgey",
+                        description: "Very gentle and loving",
+                        age:  4,
+                        sex: "Male",
+                        adopted: false,
+                        shelter_id: shelter_1.id)
+    pet_2 = Pet.create(image: "weedle.jpg",
+                        name: "Weedle",
+                        description: "Weed is a loyal and affectionate friend.",
+                        age:  2,
+                        sex: "Male",
+                        adopted: false,
+                        shelter_id: shelter_1.id)
+    visit "/pets/#{pet_1.id}"
+
+    expect(page).to have_content(pet_1.name)
+    expect(page).to have_content(pet_1.description)
+    expect(page).to have_content(pet_1.age)
+    expect(page).to have_content(pet_1.sex)
+    expect(page).to have_content(pet_1.adopted)
+
+    expect(page).to have_no_content(pet_2.name)
+  end
+end

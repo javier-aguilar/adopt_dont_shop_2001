4912195eea0537b815f1d7777a38a44b4d385794
Javier Aguilar <jvr.agx@gmail.com>
2020-03-21 23:16:37 -0600

    Add show adoptable and show pending pets links

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -42,7 +42,14 @@ class SheltersController < ApplicationController
   end
 
   def show_pets
-    @pet_num = Pet.where(:shelter_id => params[:id]).count
+    if(params["adoptable"] == "true")
+      @pets = Pet.where(:shelter_id => params[:id], :status => "Adoptable")
+    elsif(params["adoptable"] == "false")
+      @pets = Pet.where(:shelter_id => params[:id], :status => "Pending")
+    else
+      @pets = Pet.where(:shelter_id => params[:id])
+    end
+    @pet_num = @pets.size
     @shelter = Shelter.find(params[:id])
   end
 
---
+++ spec/features/extensions/user_can_see_only_adoptable_pets_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe "As a visitor", type: :feature do
+  describe "when I visit the pets index page or a shelter pets index page"
+    it "I can click a link to only show adoption-pending pets" do
+      shelter_1 = Shelter.create(name: "Pallet Town Shelter",
+                          address: "Route 1",
+                          city:  "Pallet Town",
+                          state: "Kanto",
+                          zip: "80807")
+      shelter_2 = Shelter.create(name: "Cerulean City Center",
+                          address: "Route 5",
+                          city:  "Cerulean City",
+                          state: "Kanto",
+                          zip: "80808")
+      pet_1 = Pet.create(image: "pidgey.jpg",
+                          name: "Pidgey",
+                          description: "Very gentle and loving",
+                          age:  4,
+                          sex: "Male",
+                          status: "Adoptable",
+                          shelter_id: shelter_1.id)
+      pet_2 = Pet.create(image: "weedle.jpg",
+                          name: "Weedle",
+                          description: "Weed is a loyal and affectionate friend.",
+                          age:  2,
+                          sex: "Male",
+                          status: "Pending",
+                          shelter_id: shelter_1.id)
+      visit "/shelters/#{shelter_1.id}/pets"
+
+      click_link "Show Adoptable"
+
+      expect(page).to have_content("#{pet_1.name}")
+      expect(page).to have_no_content("#{pet_2.name}")
+
+      click_link "Show Pending"
+
+      expect(page).to have_no_content("#{pet_1.name}")
+      expect(page).to have_content("#{pet_2.name}")
+
+
+    end
+end

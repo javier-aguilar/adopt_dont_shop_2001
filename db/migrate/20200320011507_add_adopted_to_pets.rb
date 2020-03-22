27d99738487643753d09141e0be11ad83dc832f1
Javier Aguilar <jvr.agx@gmail.com>
2020-03-21 19:16:10 -0600

    Update migration files

--- db/migrate/20200320011507_add_adopted_to_pets.rb
+++
@@ -1,6 +0,0 @@
-class AddStatusAndDescriptionToPets < ActiveRecord::Migration[5.1]
-  def change
-    add_column :pets, :description, :string
-    add_column :pets, :status, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200320011620) do
+ActiveRecord::Schema.define(version: 20200322011337) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20200320011620) do
     t.string "status"
     t.string "description"
     t.string "string"
+    t.string "adopted"
     t.index ["shelter_id"], name: "index_pets_on_shelter_id"
   end
 
